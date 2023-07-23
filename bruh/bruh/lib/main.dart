bf331a8d54a147c0fd00d151ed71499bcf0ca3bf
Prasanna Nage <prasannanage61@gmaqil.com>
2023-07-23 19:22:51 +0530

    mass

---
+++ bruh/bruh/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:tracker/tests/sources/home.dart';
+import 'package:tracker/tests/sources/expenses.dart';
+
+void main() => runApp(const BudgetTrackerApp());
+
+class BudgetTrackerApp extends StatefulWidget {
+  const BudgetTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  State<BudgetTrackerApp> createState() => _BudgetTrackerAppState();
+}
+
+class _BudgetTrackerAppState extends State<BudgetTrackerApp> {
+  int budget = 50000;
+  List<Item> items = [];
+
+  void updateBudget(List<Item> updatedItems) {
+    int totalCost = 0;
+    for (var item in updatedItems) {
+      totalCost += item.price;
+    }
+    setState(() {
+      budget = 50000 - totalCost;
+      items = updatedItems;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Budget Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(budget: budget),
+        '/expenses': (context) => Expenses(
+          items: items,
+          updateBudget: updateBudget,
+          budget: budget,
+        ),
+      },
+    );
+  }
+}
